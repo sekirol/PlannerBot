db46b7c52a09f31377b2237577abe262a2499e72
Sergei Kirikeza <serj280294@yandex.ru>
2022-08-05 18:35:50 +0300

    Task creation time

--- planner.py
+++ planner.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
+from datetime import datetime
+
 class Task:
     default_duration = 25
     default_group = "usual"
 
     def __init__(self, title: str, start_time: str, duration: str, group: str):
+        self._set_creation_time()
+
         self.title = title
         self.duration = duration if duration else Task.default_duration
         self.start_time = start_time
         self.group = group if group else Task.default_group
 
+    def _set_creation_time(self):
+        self.creation_time = datetime.now().isoformat(sep=' ', timespec='seconds')
+        
 class Planner:
     def __init__(self):
         self.tasks_list = []    
