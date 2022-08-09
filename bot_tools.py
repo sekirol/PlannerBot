8651dd874ebdd5e1fc64e9c5ee63fad3604fdb3c
Sergei Kirikeza <serj280294@yandex.ru>
2022-08-10 00:45:55 +0300

    New button in planning mode

--- bot_tools.py
+++ bot_tools.py
@@ -2,6 +2,7 @@
 
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 class AppState(StatesGroup):
@@ -18,7 +19,11 @@ async def cmd_start_handler(message: types.Message):
 
 async def cmd_plan_handler(message: types.Message):
     await AppState.planning.set()
-    await message.answer("Опишите создаваемую задачу")
+
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    keyboard.add("Список актуальных задач")
+
+    await message.answer("Опишите создаваемую задачу", reply_markup=keyboard)
 
 async def new_task_handler(message: types.Message):
     await message.answer(f"Создана задача:\n"
@@ -28,7 +33,11 @@ async def new_task_handler(message: types.Message):
                          f"Вы можете создать ещё одну, или выйти,\n"
                          f"используя команду /start\n", parse_mode="MarkdownV2")
 
+async def actual_list_handler(message: types.Message):
+    await message.answer("Актуальные задачи:")
+
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(cmd_start_handler, commands="start", state="*")
     dp.register_message_handler(cmd_plan_handler, commands="plan", state=AppState.initial)
+    dp.register_message_handler(actual_list_handler, Text(equals="Список актуальных задач", ignore_case=True), state=AppState.planning),
     dp.register_message_handler(new_task_handler, state=AppState.planning)
