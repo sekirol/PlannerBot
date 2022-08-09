96160992285c472ab09e29a1a1d9f995a7792c1e
Sergei Kirikeza <serj280294@yandex.ru>
2022-08-10 00:05:21 +0300

    FSM implementation

--- bot_tools.py
+++ bot_tools.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python3
 
-
 from aiogram import Dispatcher, types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+class AppState(StatesGroup):
+    initial = State()
+    planning = State()
 
 async def cmd_start_handler(message: types.Message):
-    await message.answer("Для начала работы выберите одну из команд.")
+    await AppState.initial.set()
 
-async def cmd_planning_handler(message: types.Message):
-    await message.answer("Режим планирования")
-    
-async def cmd_schedule_handler(message: types.Message):
-    await message.answer("Режим просмотра графика")
+    initial_message = "Для начала работы выберите команду:\n" \
+                      "*\/plan* \- перейти в режим планирования"
 
-async def cmd_statistic_handler(message: types.Message):
-    await message.answer("Режим просмотра статистики")
+    await message.answer(initial_message, reply_markup=types.ReplyKeyboardRemove(), parse_mode="MarkdownV2")
+
+async def cmd_plan_handler(message: types.Message):
+    await AppState.planning.set()
+    await message.answer("Опишите создаваемую задачу")
+
+async def new_task_handler(message: types.Message):
+    await message.answer(f"Создана задача:\n"
+                         f"{message.text}\n"
+                         f"Начало:\n"
+                         f"Продолжительность:\n\n"
+                         f"Вы можете создать ещё одну, или выйти,\n"
+                         f"используя команду /start\n", parse_mode="MarkdownV2")
 
 def register_handlers(dp: Dispatcher):
-    dp.register_message_handler(cmd_start_handler, commands="start")
-    dp.register_message_handler(cmd_planning_handler, commands="plan")
-    dp.register_message_handler(cmd_schedule_handler, commands="shed")
-    dp.register_message_handler(cmd_statistic_handler, commands="stat")
+    dp.register_message_handler(cmd_start_handler, commands="start", state="*")
+    dp.register_message_handler(cmd_plan_handler, commands="plan", state=AppState.initial)
+    dp.register_message_handler(new_task_handler, state=AppState.planning)
--- worker.py
+++ worker.py
@@ -4,11 +4,15 @@ import asyncio
 import bot_tools
 
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
 from tools import get_access_data
 
 async def main():
     bot = Bot(get_access_data("telegram"))
-    dp = Dispatcher(bot)
+
+    storage = MemoryStorage()
+    dp = Dispatcher(bot, storage=storage)
 
     bot_tools.register_handlers(dp)
 
