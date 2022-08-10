afe3c34490afd939d81f6a11d2bfd7728bc94708
Sergei Kirikeza <serj280294@yandex.ru>
2022-08-10 13:08:12 +0300

    New bot menu

--- bot_tools.py
+++ bot_tools.py
@@ -7,37 +7,34 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 class AppState(StatesGroup):
     initial = State()
+    create = State()
+    edit = State()
     planning = State()
 
 async def cmd_start_handler(message: types.Message):
     await AppState.initial.set()
 
     initial_message = "Для начала работы выберите команду:\n" \
-                      "*\/plan* \- перейти в режим планирования"
+                      "*\/new* \- создание задачи\n" \
+                      "*\/edit* \- редактирование задач"
 
     await message.answer(initial_message, reply_markup=types.ReplyKeyboardRemove(), parse_mode="MarkdownV2")
 
-async def cmd_plan_handler(message: types.Message):
-    await AppState.planning.set()
+async def cmd_new_handler(message: types.Message):
+    await AppState.create.set()
+    await message.answer("Назовите задачу")
 
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-    keyboard.add("Список актуальных задач")
-
-    await message.answer("Опишите создаваемую задачу", reply_markup=keyboard)
+async def cmd_edit_handler(message: types.Message):
+    await AppState.edit.set()
+    await message.answer("Выберите задачу для редактирования")
 
 async def new_task_handler(message: types.Message):
-    await message.answer(f"Создана задача:\n"
-                         f"{message.text}\n"
-                         f"Начало:\n"
-                         f"Продолжительность:\n\n"
-                         f"Вы можете создать ещё одну, или выйти,\n"
-                         f"используя команду /start\n", parse_mode="MarkdownV2")
-
-async def actual_list_handler(message: types.Message):
-    await message.answer("Актуальные задачи:")
+    await message.answer("Создана задача:\n"
+                        f"{message.text}")
 
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(cmd_start_handler, commands="start", state="*")
-    dp.register_message_handler(cmd_plan_handler, commands="plan", state=AppState.initial)
-    dp.register_message_handler(actual_list_handler, Text(equals="Список актуальных задач", ignore_case=True), state=AppState.planning),
-    dp.register_message_handler(new_task_handler, state=AppState.planning)
+    dp.register_message_handler(cmd_new_handler, commands="new", state=AppState.initial)
+    dp.register_message_handler(cmd_edit_handler, commands="edit", state=AppState.initial)
+
+    dp.register_message_handler(new_task_handler, state=AppState.create)
