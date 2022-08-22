8eb8a4f9891b8da564a9accbbbc2ebdce45cb6ce
Sergei Kirikeza <serj280294@yandex.ru>
2022-08-22 23:44:33 +0300

    State info of new task

--- app/bot/handlers/create.py
+++ app/bot/handlers/create.py
@@ -1,4 +1,6 @@
 
+import pprint
+
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
 
@@ -19,7 +21,25 @@ class CreateMenuHandlers:
         
     async def cmd_create(self, message: types.Message, state: FSMContext) -> None:
         await state.set_state(AppState.create)
+
+        task_info = {
+            "title": None,
+            "group": None,
+            "duration": None,
+            "start": None
+        }
+        await state.set_data(task_info)
+
         await message.answer(messages.create)
 
+    async def new_task(self, message: types.Message, state: FSMContext) -> None:
+        task_info = await state.get_data()
+
+        task_info["title"] = message.text
+
+        await message.answer(pprint.pformat(task_info, width=1, sort_dicts=False))
+        await state.update_data(task_info)
+        
     def _register_handlers(self) -> None:
         self.dp.register_message_handler(self.cmd_create, commands="new", state=AppState.initial)
+        self.dp.register_message_handler(self.new_task, state=AppState.create)
