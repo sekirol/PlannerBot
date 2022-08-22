d0d8dea4a446a574c42eac6b356a56e849b782a5
Sergei Kirikeza <serj280294@yandex.ru>
2022-08-22 22:20:45 +0300

    Now common handlers is BasicHandlers class

---
+++ app/bot/handlers/basic.py
@@ -0,0 +1,28 @@
+
+from aiogram import Dispatcher, types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+
+from ..state_machine import AppState
+
+from . import messages
+
+class BasicHandlers:
+    """
+    Basic commands handlers
+    """
+
+    def __init__(self, dp) -> None:
+        self.dp = dp
+
+        self._register_handlers()
+
+    async def cmd_start(message: types.Message, state: FSMContext) -> None:
+        await state.finish()
+        await state.set_state(AppState.initial)
+        await message.answer(messages.initial, reply_markup=types.ReplyKeyboardRemove())
+
+    def _register_handlers(self) -> None:
+        self.dp.register_message_handler(self.cmd_start, commands="start", state="*")
+        self.dp.register_message_handler(self.cmd_start, commands="cancel", state="*")
+        self.dp.register_message_handler(self.cmd_start, Text(equals="отмена", ignore_case=True), state="*")
