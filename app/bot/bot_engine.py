f3c2712e87f007163023bfe3bf7cd73dc0f9f578
Sergei Kirikeza <serj280294@yandex.ru>
2022-08-25 00:31:40 +0600

    FSM storage path fixed

--- app/bot/bot_engine.py
+++ app/bot/bot_engine.py
@@ -2,7 +2,7 @@
 
 from aiogram import Bot, Dispatcher
 from aiogram.utils import executor
-from aiogram.contrib.fsm_storage.files import PickleStorage
+from aiogram.contrib.fsm_storage.files import JSONStorage
 
 from .handlers.basic import BasicHandlers
 from .handlers.create import CreateMenuHandlers
@@ -10,7 +10,7 @@ from .handlers.create import CreateMenuHandlers
 from ..tools import get_access_data
 from ..planner import Planner
 
-FSM_STORAGE_PATH = "../data/app_state.bin"
+FSM_STORAGE_PATH = "./app/data/app_state.json"
 
 async def shutdown(dp: Dispatcher):
     await dp.storage.close()
@@ -18,7 +18,7 @@ async def shutdown(dp: Dispatcher):
 class BotEngine:
     def __init__(self):
         bot = Bot(get_access_data("telegram"))
-        storage = PickleStorage(FSM_STORAGE_PATH)
+        storage = JSONStorage(FSM_STORAGE_PATH)
         
         self.dp = Dispatcher(bot, storage=storage)
 
