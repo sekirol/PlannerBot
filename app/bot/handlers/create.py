9659e512c36e6d04f017919896ac41c352291a93
Sergei Kirikeza <serj280294@yandex.ru>
2022-08-25 00:33:08 +0600

    Inline keyboard in task group selecting menu

--- app/bot/handlers/create.py
+++ app/bot/handlers/create.py
@@ -35,11 +35,21 @@ class CreateMenuHandlers:
     async def new_task(self, message: types.Message, state: FSMContext) -> None:
         task_info = await state.get_data()
 
-        task_info["title"] = message.text
-
-        await message.answer(pprint.pformat(task_info, width=1, sort_dicts=False))
+        task_info["title"] = message.text.capitalize()
         await state.update_data(task_info)
+
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(types.InlineKeyboardButton("Test", callback_data="test"))
+
+        answer_text = f"<b>{task_info['title']}</b>\n\n" \
+                      "Выберите категорию задачи:"
+
+        await message.answer(answer_text, parse_mode="HTML", reply_markup=keyboard)
         
+    async def task_edit_callback(self, query: types.CallbackQuery):
+        await query.answer(query.data)
+ 
     def _register_handlers(self) -> None:
         self.dp.register_message_handler(self.cmd_create, commands="new", state=AppState.initial)
         self.dp.register_message_handler(self.new_task, state=AppState.create)
+        self.dp.register_callback_query_handler(self.task_edit_callback, state=AppState.create)
